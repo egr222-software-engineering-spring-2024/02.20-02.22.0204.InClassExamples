1f02a83abdf2b983ded7d18fe85eb56250d23679
Larry Clement <lclement@calbaptist.edu>
2024-02-23 18:46:00 -0800

    After fourth video

--- A/Point/src/Point.java
+++ A/Point/src/Point.java
@@ -2,12 +2,78 @@ import java.awt.*;
 
 // TO DO:  Implement a class of our own named Point
 public class Point {
-    int x;
-    int y;
+    private int x;
+    private int y;
+
+    private static Point earthquakeOrigin;
+    private static int earthquakeImpactRadius = 50;
+
+    public Point() {
+        this(0, 0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static void setEarthquakeOrigin(Point p) {
+        earthquakeOrigin = p;
+    }
+
+    public static void setEarthquakeImpactRadius(int radius) {
+        earthquakeImpactRadius = radius;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 
     public void draw(Graphics g) {
+        if (earthquakeOrigin != null && distanceFromEarthquakeOrigin() < earthquakeImpactRadius) {
+            g.setColor(Color.red);
+        }
+        else g.setColor(Color.black);
+
         g.fillOval(x, y, 3, 3);
         g.drawString("(" + x + ", " + y + ")", x, y);
+    }
+
+    public double distanceFromEarthquakeOrigin() {
+        int deltaX = x - earthquakeOrigin.x;
+        int deltaY = y - earthquakeOrigin.y;
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+
+    public static void drawEarthquakeOrigin(Graphics g) {
+        if (earthquakeOrigin != null) {
+            Color c = g.getColor();
+
+            g.setColor(Color.red);
+//            earthquakeOrigin.draw(g);
+            g.fillOval(earthquakeOrigin.x, earthquakeOrigin.y, 3,3);
+
+            int earthquakeCircleRadius = 5;
+            while (earthquakeCircleRadius <= earthquakeImpactRadius) {
+                int diameter = 2 * earthquakeCircleRadius;
+                g.drawOval(earthquakeOrigin.x - earthquakeCircleRadius, earthquakeOrigin.y - earthquakeCircleRadius, diameter, diameter);
+                earthquakeCircleRadius += 5;
+            }
+
+            g.setColor(c);
+        }
     }
 
     @Override
--- A/Point/src/PointClient.java
+++ A/Point/src/PointClient.java
@@ -5,16 +5,30 @@ public class PointClient {
         DrawingPanel panel = new DrawingPanel();
         Graphics g = panel.getGraphics();
 
-        Point p1 = new Point();
-        p1.x = 15;
-        p1.y = 37;
+        Point.setEarthquakeOrigin(new Point(80,40));
+        Point.setEarthquakeImpactRadius(50);
+
+        Point.drawEarthquakeOrigin(g);
+
+        Point p1 = new Point(15, 37);
+//        p1.x = 15;
+//        p1.y = 37;
 
         System.out.println("Point 1 is " + p1);
-//        p1.draw(g);
+        System.out.println("Point 1.x is " + p1.getX());
+        System.out.println("Point 1.y is " + p1.getY());
+        p1.draw(g);
 //
-//        Point p2 = new Point();
+        Point p2 = new Point(51, 79);
 //        p2.x = 51;
 //        p2.y = 79;
-//        p2.draw(g);
+        System.out.println("Point 2 is " + p2);
+        p2.draw(g);
+//
+        Point p3 = new Point();
+        p3.setX(150);
+        p3.setY(200);
+        System.out.println("Point 3 is " + p3);
+        p3.draw(g);
     }
 }
